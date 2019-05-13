1b12568c2e03a1e76be0c099a2b010d7530d935c
Mateo <mateofreyre1996@gmail.com>
2019-05-13 15:25:48 -0300

    permisos

--- controller/permission.php
+++ controller/permission.php
@@ -1,6 +1,6 @@
 <?php
 
-	$permission_admin = array("home", "ver_perfil", "logout");
+	$permission_admin = array();
 	$permission_user = array("home", "logout", "ver_perfil", "agregar_tarjeta", "agregar_tarjeta_check", "agregar_vehiculo", "agregar_vehiculo_check", "listar_vehiculos", "modificar_vehiculo", "modificar_vehiculo_check", "listar_tarjetas", "eliminar_tarjeta_check", "agregar_viaje", "chequear_agregar_viaje");
 	$permission_visitante = array("home", "loguearUsuario", "agregar_usuario", "login", "chequear_agregar_usuario", "login_user_check", "agregar_tarjeta", "agregar_tarjeta_check");
 
