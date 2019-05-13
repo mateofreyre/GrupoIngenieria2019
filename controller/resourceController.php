4c075ff6ad544ffc892f40d8d8ec095b40bc3934
Mateo <mateofreyre1996@gmail.com>
2019-05-13 16:47:32 -0300

    Agregar propiedad

--- controller/resourceController.php
+++ controller/resourceController.php
@@ -5,7 +5,6 @@ class ResourceController {
 
 	private static $instance;
 	public static function getInstance() {
-
         if (!isset(self::$instance)) {
             self::$instance = new self();
         }
