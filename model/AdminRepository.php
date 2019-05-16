007913b663bc3223b9fdd96eee0383e40cec3fd1
zemaitismarcos <marcos87lp@gmail.com>
2019-05-15 22:56:58 -0300

    agregue el admin repository

---
+++ model/AdminRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+Class AdminRepository extends PDORepository {
+
+	private static $instance;
+
+	public static function getInstance() {
+		if (!isset(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct {
+
+	}
+
+	public function chequear_codigo($codigo){
+  		$query = self::queryAll("SELECT * FROM Administrador WHERE codigo = '{$codigo}'");
+    	$res = $query->fetch(PDO::FETCH_ASSOC);
+    	if(!empty($res)){
+    		$query = null;
+      	$mensaje = "Codigo incorrecto, intentelo otra vez.";
+      	echo "<script>";
+      	echo "alert('$mensaje');";
+      	echo "</script>";
+      	return false;
+    	}
+    	$query = null;
+    	return true;
+  }
+
+?>
