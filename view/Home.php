d0891bb41d2a2f7c447d4f68da19892e17d8710b
Mateo <mateofreyre1996@gmail.com>
2019-05-15 23:07:52 -0300

    Home - inicio de sesion admin

---
+++ view/Home.php
@@ -0,0 +1,14 @@
+<?php
+class Home extends TwigView {
+
+  public function show() {
+
+      $templateDir="./templates";
+      $loader = new Twig_Loader_Filesystem($templateDir);
+      $twig = new Twig_Environment($loader);
+      $template = $twig->loadTemplate("inicio.html.twig");
+      $template->display(array('rol' => $_SESSION['rol']));
+    }
+
+}
+?>
